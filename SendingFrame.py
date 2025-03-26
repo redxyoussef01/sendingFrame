aa9aac0003db7ef6ad057f4a2be0c5b89dddb35e
redxyoussef01 <yourais00@gmail.com>
2025-03-26 22:59:07 +0000

    url

--- SendingFrame.py
+++ SendingFrame.py
@@ -1,13 +1,18 @@
 import cv2
 import requests
 
+# Replace with your ngrok URL
+ngrok_url = 'https://76a7-41-143-49-120.ngrok-free.app'
+
 cap = cv2.VideoCapture("drive.mp4")
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
     _, encoded_frame = cv2.imencode('.jpg', frame)
-    response = requests.post('http://127.0.0.1:4002/video_frame', data=encoded_frame.tobytes())
+    response = requests.post(f'{ngrok_url}/video_frame', data=encoded_frame.tobytes())
     print(response.json())
 cap.release()
-requests.post('http://127.0.0.1:4002/shutdown')
+
+# Sending shutdown signal
+requests.post(f'{ngrok_url}/shutdown')
