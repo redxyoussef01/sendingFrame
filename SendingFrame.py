cf4b2ca147168c77a1b39c107e04f2eeddaab039
redxyoussef01 <yourais00@gmail.com>
2025-03-29 13:09:11 +0000

    a

--- SendingFrame.py
+++ SendingFrame.py
@@ -3,7 +3,7 @@ import cv2
 import requests
 
 ngrok_url = 'https://458f-41-251-195-136.ngrok-free.app'
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('/dev/video0')
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
