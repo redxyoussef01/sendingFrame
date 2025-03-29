86de257418b6752a1e7f433d83888a732f1c63e7
redxyoussef01 <yourais00@gmail.com>
2025-03-29 13:18:03 +0000

    a

--- SendingFrame.py
+++ SendingFrame.py
@@ -1,12 +1,8 @@
 import cv2
 
 cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L2)
+  # Try with default device
 
-# Set a lower resolution, e.g., 320x240
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)  # Set width to 320
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)  # Set height to 240
-
-# Check if the camera is opened
 if not cap.isOpened():
     print("Error: Could not open camera.")
 else:
