9d07da7688e06a94e7e2338b18a2ea5f1e3c5e97
redxyoussef01 <yourais00@gmail.com>
2025-03-29 12:53:00 +0000

    a

--- SendingFrame.py
+++ SendingFrame.py
@@ -1,31 +1,15 @@
+
 import cv2
 import requests
 
-# Replace with your ngrok URL
 ngrok_url = ' https://458f-41-251-195-136.ngrok-free.app'
-
-for source in range(0, 41):
-    print(f"Testing source {source}...")
-    cap = cv2.VideoCapture(source)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    
-    if not cap.isOpened():
-        print(f"Source {source} is not available.")
-        continue  # Skip to the next source
-
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
-        print(f"Source {source} could not capture a frame.")
-        cap.release()
-        continue
-    
+        break
     _, encoded_frame = cv2.imencode('.jpg', frame)
     response = requests.post(f'{ngrok_url}/video_frame', data=encoded_frame.tobytes())
-    
-    print(f"Source {source} response:", response.json())
-
-    cap.release()
-
-# Sending shutdown signal
+    print(response.json())
+cap.release()
 requests.post(f'{ngrok_url}/shutdown')
