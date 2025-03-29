74c53ca8b831007c59ce946163224c6d592c45a6
redxyoussef01 <yourais00@gmail.com>
2025-03-29 13:14:28 +0000

    a

--- SendingFrame.py
+++ SendingFrame.py
@@ -1,33 +1,21 @@
 import cv2
-import requests
 
-ngrok_url = 'https://458f-41-251-195-136.ngrok-free.app'
+cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L2)
 
-# Open video capture from /dev/video0 (the camera device)
-cap = cv2.VideoCapture('/dev/video0')
+# Set a lower resolution, e.g., 320x240
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)  # Set width to 320
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)  # Set height to 240
 
+# Check if the camera is opened
 if not cap.isOpened():
     print("Error: Could not open camera.")
-    exit()
-
-# Set the resolution of the captured frames (e.g., 640x480)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)  # Set width
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)  # Set height
-
-while cap.isOpened():
+else:
     ret, frame = cap.read()
     if not ret:
         print("Error: Failed to capture frame.")
-        break
-
-    # Encode the frame as JPEG
-    _, encoded_frame = cv2.imencode('.jpg', frame)
-
-    # Send the frame to your server
-    response = requests.post(f'{ngrok_url}/video_frame', data=encoded_frame.tobytes())
-    print(response.json())
+    else:
+        cv2.imshow("Frame", frame)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 cap.release()
-
-# Send shutdown signal to the server
-requests.post(f'{ngrok_url}/shutdown')
