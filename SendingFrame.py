b977ef9201683a077b636660765e6852a63edd20
redxyoussef01 <yourais00@gmail.com>
2025-04-02 12:10:20 +0000

    aa

--- SendingFrame.py
+++ SendingFrame.py
@@ -1,13 +1,23 @@
 import cv2
 import requests
 
+URL = 'https://963e-105-157-85-231.ngrok-free.app/video_frame'
+
 cap = cv2.VideoCapture(0)
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
+
     _, encoded_frame = cv2.imencode('.jpg', frame)
-    response = requests.post('https://963e-105-157-85-231.ngrok-free.app/video_frame', data=encoded_frame.tobytes())
-    print(response.json())
+
+    try:
+        response = requests.post(URL, data=encoded_frame.tobytes(), verify=False, timeout=5)
+        print(response.status_code, response.text)
+    except requests.exceptions.SSLError as e:
+        print("SSL Error:", e)
+    except requests.exceptions.RequestException as e:
+        print("Request Error:", e)
+
 cap.release()
-requests.post('https://963e-105-157-85-231.ngrok-free.app/shutdown')
+requests.post('https://963e-105-157-85-231.ngrok-free.app/shutdown', verify=False)
