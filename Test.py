c545879d6a9d39bd86a31e453cd4d456fa8e6193
redxyoussef01 <yourais00@gmail.com>
2025-04-05 13:56:37 +0000

    a

--- Test.py
+++ Test.py
@@ -527,7 +527,7 @@ def demo_sequence(robot):
         # Optional calibration
         #robot.calibrate_motors()
         
-        #robot.accelerate(200)
+        robot.accelerate(200)
         time.sleep(2)
         
         robot.arc('left', 90, -100)
