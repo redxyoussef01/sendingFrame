ab360193a66154541e3c5569dc3323947d60c67f
redxyoussef01 <yourais00@gmail.com>
2025-04-05 14:11:01 +0000

    a

--- Test.py
+++ Test.py
@@ -530,7 +530,7 @@ def demo_sequence(robot):
         robot.accelerate(120)
         time.sleep(1.2)
         
-        robot.arc('right', 90,  -360)
+        robot.arc('right', 110,  -360)
         time.sleep(2.5)
        
        
